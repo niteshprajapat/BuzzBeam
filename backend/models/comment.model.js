dd456a3845feeb74ae0d3f150dca61fffd79aa18
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-06 11:57:03 +0530

    comment model added

--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -15,8 +15,12 @@ const commentSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-
-
+    commentLikedBy: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
 
 
 }, { timestamps: true });
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 
-import { login, logout, register, uploadMedia, getProfileById, searchAccount, updateProfile } from '../controllers/user.controller.js';
+import { login, logout, register, uploadMedia, getProfileById, searchAccount, updateProfile, deleteAccount, followUnfollow, followingList, followersList, suggestedUsers } from '../controllers/user.controller.js';
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
 // import { uploadFile } from '../middlewares/multer.js';
 
@@ -16,10 +16,17 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', isAuthenticated, logout);
 router.get('/profile/:id', isAuthenticated, getProfileById);
-router.put('/updateProfile', isAuthenticated, updateProfile);
+router.put('/updateProfile', isAuthenticated, updateProfile);   // only loggedIn user can update their own profile
+router.put('/delete', isAuthenticated, deleteAccount);   // only loggedIn user can delete their own account
 router.get('/search', isAuthenticated, searchAccount);
 
+// follow-unfollow
+router.get('/followUnfollow/:id', isAuthenticated, followUnfollow);
 
+// Follower-Follwing list of user by Id
+router.get('/followingList/:id', isAuthenticated, followingList);
+router.get('/followersList/:id', isAuthenticated, followersList);
+router.get('/suggestedUsers', isAuthenticated, suggestedUsers);
 
 
 
