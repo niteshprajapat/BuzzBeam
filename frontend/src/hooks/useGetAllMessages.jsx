b938b1e9271c20a6720295a3050c1e039e0a864d
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-12 22:37:42 +0530

    hook for get all messages

---
+++ frontend/src/hooks/useGetAllMessages.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { setFollowing } from '@/redux/slices/userSlice';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMessages } from '@/redux/slices/messageSlice';
+
+const useGetAllMessages = () => {
+    const dispatch = useDispatch();
+    const { token, selectedUser } = useSelector((store) => store?.auth);
+
+    const fetchAllMessages = async () => {
+        try {
+            const response = await axios.get(`http://127.0.0.1:5000/api/v1/messages/getMessages/${selectedUser?._id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + token
+                },
+                withCredentials: true,
+            });
+
+            const data = await response.data;
+
+            console.log("getAllMessages", data);
+
+            if (data?.success) {
+                dispatch(setMessages(data?.messages));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchAllMessages();
+    }, []);
+}
+
+
+
+export default useGetAllMessages;
