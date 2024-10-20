353661fdc5084dd822f6776e9f42895be94631b7
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-20 19:27:37 +0530

    working on notification functinality

---
+++ backend/models/notification.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    to: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    type: {
+        type: String,
+        enum: ["follow", "like", "comment"],
+    },
+    isRead: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+
+
+}, { timestamps: true });
+
+
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;
