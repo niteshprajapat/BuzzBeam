20d3cd18f101d966c56b06be5b4f40ec9b36a60e
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-22 23:01:18 +0530

    created notification page in frontend side

---
+++ frontend/src/pages/Notifications/Notifications.jsx
@@ -0,0 +1,20 @@
+import Sidebar from '@/components/ui/Sidebar/Sidebar'
+import React from 'react'
+
+
+const Notifications = () => {
+    return (
+        <div className='flex h-screen'>
+
+            <Sidebar />
+
+            <div className='flex flex-col w-full  h-full overflow-y-auto p-10 '>
+
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Notifications
