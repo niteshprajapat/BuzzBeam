f59d8edf94f8262d5bce331a31e0f124ff61de69
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-23 17:41:49 +0530

    Story routes created

---
+++ backend/routes/story.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const router = express.Router();
+
+
+import { isAuthenticated } from '../middlewares/authMiddleware.js';
+import { } from '../controllers/story.controller.js';
+
+
+// Routes
+
+
+
+export default router;
