833a651a35a56130cd901e391e3f345277618383
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-23 13:25:50 +0530

    Created  Notification APIs

--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getAllNotifications } from '../controllers/notification.controller.js';
+import { deleteAllNotifications, deleteNotificationById, getAllNotifications, readAllNotifications, readNotifiicationById } from '../controllers/notification.controller.js';
 
 
 
@@ -15,8 +15,17 @@ router.get("/getAllNotifications", isAuthenticated, getAllNotifications);
 // Mark Notification as Read
 // Mark All Notification as Read
 
+router.get("/readNotification/:id", isAuthenticated, readNotifiicationById);
+router.get("/readAllNotifications", isAuthenticated, readAllNotifications);
+
+
 // Delete Notifiaction by Id
 // Delete All Notifiaction of LoggedIn User
+
+router.delete("/deleteNotification/:id", isAuthenticated, deleteNotificationById);
+router.delete("/deleteAllNotifications", isAuthenticated, deleteAllNotifications);
+
+
 
 
 
