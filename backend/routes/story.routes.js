fa9fdff09864d0eb70c81e84dfc0539d22076cad
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-24 19:06:02 +0530

    Story APIs route

--- backend/routes/story.routes.js
+++ backend/routes/story.routes.js
@@ -3,11 +3,26 @@ const router = express.Router();
 
 
 import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { } from '../controllers/story.controller.js';
+import { deleteStoryById, getAllFollowingsStory, storyViewedBy, uploadMedia, uploadStory, viewStory } from '../controllers/story.controller.js';
 
 
 // Routes
 
+router.post('/uploadFile', uploadMedia);
+
+
+router.post("/uploadStory", isAuthenticated, uploadStory);
+router.get("/getAllFollowingsStory", isAuthenticated, getAllFollowingsStory);
+
+
+// delete story
+router.delete("/deleteStoryById/:id", isAuthenticated, deleteStoryById);
+
+// remove story after 24hrs -> Done by Cron job
+
+// story viewers
+router.get("/viewStory/:id", isAuthenticated, viewStory);
+router.get("/storyViewedBy/:id", isAuthenticated, storyViewedBy);
 
 
 export default router;
