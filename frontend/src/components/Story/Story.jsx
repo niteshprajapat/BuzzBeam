5043135d5adfc1dbd3c67343c17f79f3110f3522
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-31 09:25:20 +0530

    working on story view, delete fuctionalities

--- frontend/src/components/Story/Story.jsx
+++ frontend/src/components/Story/Story.jsx
@@ -4,157 +4,74 @@ import { useSelector } from 'react-redux';
 import { Button } from '../ui/button';
 import axios from 'axios';
 import { BACKEND_URL } from '@/route';
+import UploadStoryModal from './UploadStoryModal';
+import ViewYourStoryModal from './ViewYourStoryModal';
 
 const Story = () => {
-    const { stories } = useSelector((store) => store?.story);
+    const { stories, yourStory } = useSelector((store) => store?.story);
+
+
 
     useGetAllStories();
 
     console.log("stories", stories);
 
-    const [file, setFile] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
-    const imageRef = useRef();
-    const [filePreview, setFilePreview] = useState(null);
 
 
-
-    const readFileAsDataUrl = async (file) => {
-        const fileUrlData = new Promise((resolve, reject) => {
-            const reader = new FileReader();
-
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = () => reject(new Error("Error reading file"));
-
-            reader.readAsDataURL(file);
-        });
-
-        return fileUrlData;
-    }
-
-    const handleUploadPost = async (e) => {
-        try {
-
-            if (e.target.files && e.target.files[0]) {
-                const file = e.target.files[0];
-
-                if (file) {
-                    const dataUri = await readFileAsDataUrl(file);
-                    setFilePreview(dataUri);
-                }
-
-                try {
-                    const response = await axios.post(`${BACKEND_URL}/api/v1/story/uploadFile`, {
-                        media: file
-                    }, {
-                        headers: {
-                            'Content-Type': 'multipart/form-data',
-                        },
-                        withCredentials: true,
-                    });
-
-                    const data = await response.data;
-
-                    if (data?.success) {
-                        setImageUrl(data?.url);
-                    }
-
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    // const handleUploadPost = async (e) => {
-    //     if (e.target.files && e.target.files[0]) {
-    //         const file = e.target.files[0];
-    //         if (file) {
-    //             setFile(file);
-
-    //             const dataUri = await readFileAsDataUrl(file);
-    //             setFilePreview(dataUri);
-    //         }
+    const [openModal, setOpenModal] = useState(false);
+    const [yourStoryopenModal, setYourStoryOpenModal] = useState(false);
 
 
-    //         try {
-    //             const response = await axios.post(`${BACKEND_URL}/api/v1/story/uploadFile`, {
-    //                 media: file
-    //             }, {
-    //                 headers: {
-    //                     'Content-Type': 'multipart/form-data',
-    //                 },
-    //                 withCredentials: true,
-    //             });
-
-    //             console.log("responseresponse:", response);
-
-    //             const data = await response.data;
-
-    //             if (data?.success) {
-    //                 console.log("pictureda,", data);
-    //                 setImageUrl(data?.url);
-    //             }
-
-
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    // }
 
 
 
     return (
-        <div className='p-5 flex flex-col '>
+        <>
+            <UploadStoryModal openModal={openModal} setOpenModal={setOpenModal} />
+            <ViewYourStoryModal yourStoryopenModal={yourStoryopenModal} setYourStoryOpenModal={setYourStoryOpenModal} />
 
-            <div>
-                <div className='h-[300px]  flex justify-center items-center w-fit'>
-                    <input
-                        onChange={handleUploadPost}
-                        ref={imageRef}
-                        type="file"
-                        className='hidden'
-                    />
 
-                    <Button
-                        onClick={() => imageRef.current.click()}
-                        type="button"
-                        variant=""
-                        className="text-white bg-blue-500"
-                    >Choose Story from Computer</Button>
-                </div>
-            </div>
+            <div className='p-5 flex flex-col '>
 
-            {/* Need to fix this  */}
-            {
-                stories && stories?.map((story) => (
-                    <div className='size-16 rounded-full bg-gray-100 opacity-100' key={story?._id}>
-                        <div>
-                            {
-                                story?.storyType === "image" ? (
-                                    <img
-                                        src={story.storySource}
-                                        alt="story"
-                                    />
-                                )
-                                    : (
-                                        <video
-                                            src={story?.storySource}
-                                            controls
+
+                {
+                    yourStory?.storySource ? (
+                        <button onClick={() => setYourStoryOpenModal(!yourStoryopenModal)} >View Your Story</button>
+                    ) : (
+                        <button onClick={() => setOpenModal(!openModal)} >Add Story</button>
+                    )
+                }
+
+
+
+
+                {/* Need to fix this  */}
+                {
+                    stories && stories?.map((story) => (
+                        <div className='size-16 rounded-full bg-gray-100 opacity-100' key={story?._id}>
+                            <div>
+                                {
+                                    story?.storyType === "image" ? (
+                                        <img
+                                            src={story.storySource}
+                                            alt="story"
                                         />
                                     )
-                            }
+                                        : (
+                                            <video
+                                                src={story?.storySource}
+                                                controls
+                                            />
+                                        )
+                                }
+                            </div>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
 
 
-        </div>
+            </div>
+        </>
     )
 }
 
