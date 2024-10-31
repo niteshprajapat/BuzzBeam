0031001a80a1e28bdb5ce115f089e4a41ea20013
Nitesh Prajapat <niteshp282000@gmail.com>
2024-10-31 09:27:09 +0530

    added yourStory slice for loggedIn user  for story

--- frontend/src/redux/slices/storySlice.js
+++ frontend/src/redux/slices/storySlice.js
@@ -4,10 +4,14 @@ const storySlice = createSlice({
     name: "story",
     initialState: {
         stories: [],
+        yourStory: {},
     },
     reducers: {
         setStories: (state, action) => {
             state.stories = action.payload
+        },
+        setYourStory: (state, action) => {
+            state.yourStory = action.payload
         },
 
     },
@@ -15,5 +19,5 @@ const storySlice = createSlice({
 });
 
 
-export const { setStories } = storySlice.actions;
+export const { setStories, setYourStory } = storySlice.actions;
 export default storySlice.reducer;
