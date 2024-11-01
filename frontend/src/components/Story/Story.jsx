cb664068b4e08de61b31877ac2336433b89821c5
Nitesh Prajapat <niteshp282000@gmail.com>
2024-11-01 23:03:21 +0530

    working on story apis

--- frontend/src/components/Story/Story.jsx
+++ frontend/src/components/Story/Story.jsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { BACKEND_URL } from '@/route';
 import UploadStoryModal from './UploadStoryModal';
 import ViewYourStoryModal from './ViewYourStoryModal';
+import ViewOtherUsersStoryModal from './viewOtherUsersStoryModal';
 
 const Story = () => {
     const { stories, yourStory } = useSelector((store) => store?.story);
@@ -20,6 +21,8 @@ const Story = () => {
 
     const [openModal, setOpenModal] = useState(false);
     const [yourStoryopenModal, setYourStoryOpenModal] = useState(false);
+    const [otherStoryOpenModal, setOtherStoryOpenModal] = useState(false);
+    const [selectedStory, setSelectedStory] = useState({});
 
 
 
@@ -29,6 +32,7 @@ const Story = () => {
         <>
             <UploadStoryModal openModal={openModal} setOpenModal={setOpenModal} />
             <ViewYourStoryModal yourStoryopenModal={yourStoryopenModal} setYourStoryOpenModal={setYourStoryOpenModal} />
+            <ViewOtherUsersStoryModal selectedStory={selectedStory} otherStoryOpenModal={otherStoryOpenModal} setOtherStoryOpenModal={setOtherStoryOpenModal} />
 
 
             <div className='p-5 flex flex-col '>
@@ -45,29 +49,42 @@ const Story = () => {
 
 
 
-                {/* Need to fix this  */}
-                {
-                    stories && stories?.map((story) => (
-                        <div className='size-16 rounded-full bg-gray-100 opacity-100' key={story?._id}>
-                            <div>
-                                {
-                                    story?.storyType === "image" ? (
-                                        <img
-                                            src={story.storySource}
-                                            alt="story"
-                                        />
-                                    )
-                                        : (
-                                            <video
-                                                src={story?.storySource}
-                                                controls
+                <div className='flex flex-col gap-3'>
+
+
+                    {/* Need to fix this  */}
+                    {
+                        stories && stories?.map((story) => (
+                            <div className=' rounded-full bg-gray-100 opacity-100' key={story?._id}>
+                                <div onClick={() => {
+                                    setOtherStoryOpenModal(!otherStoryOpenModal)
+                                    setSelectedStory(story)
+                                }}
+                                    className='cursor-pointer' >
+                                    {
+                                        story?.storyType === "image" ? (
+                                            <img
+                                                src={story.storySource}
+                                                alt="story"
+                                                className='w-[70px] h-[70px] rounded-full object-fill'
                                             />
                                         )
-                                }
+                                            : (
+                                                <video
+                                                    src={story?.storySource}
+
+                                                    className='w-[70px] h-[70px] rounded-full object-fill'
+                                                />
+                                            )
+                                    }
+
+                                    <span>{story.user?.userName}</span>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                }
+                        ))
+                    }
+
+                </div>
 
 
             </div>
